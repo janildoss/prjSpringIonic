019b29bd5ebfa5ca84fbc7c3d906b565e602e8db
janildoss <janildoucsalinfo@gmail.com>
2022-06-30 19:49:23 -0300

    Projeto criado

---
+++ src/main/java/com/janildosantos/prjSpringIonic/PrjSpringIonicApplication.java
@@ -0,0 +1,13 @@
+package com.janildosantos.prjSpringIonic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjSpringIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjSpringIonicApplication.class, args);
+	}
+
+}

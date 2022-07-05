946629898bed0b01d259e36a1b70a3ecc000e678
janildoss <janildoucsalinfo@gmail.com>
2022-07-04 23:41:55 -0300

    Testando a primeira classe de dominio - Categoria

---
+++ src/main/java/com/janildosantos/prjSpringIonic/domain/Categoria.java
@@ -0,0 +1,57 @@
+package com.janildosantos.prjSpringIonic.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String Nome;
+	
+	public Categoria() {
+		
+	}
+
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		Nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
